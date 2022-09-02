e5f458cc6697b7a53878be7ed27b79c8f08c298a
poychang <poypost@gmail.com>
2022-09-02 14:00:04 +0800

    update app

--- apps/recit-from-memory/js/app.js
+++ apps/recit-from-memory/js/app.js
@@ -1,3 +1,4 @@
+/*===== Service Worker =====*/
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('service-worker.js')
@@ -5,37 +6,31 @@ if ('serviceWorker' in navigator) {
         .catch((err) => console.log('Service worker not registered. This happened: ', err));
 }
 
-let fontSize = 2;
-
+/*===== Resize content font size =====*/
+let fontSize = 4;
 const fontSizeUp = () => {
-    document.getElementById('title').classList.remove(`is-size-${fontSize + 1}-mobile`);
-    document.getElementById('subtitle').classList.remove(`is-size-${fontSize + 3}-mobile`);
-    document.getElementById('content').classList.remove(`is-size-${fontSize + 2}-mobile`);
+    if (fontSize <= 1) return;
 
-    document.getElementById('title').classList.add(`is-size-${fontSize - 0}-mobile`);
-    document.getElementById('subtitle').classList.add(`is-size-${fontSize + 2}-mobile`);
-    document.getElementById('content').classList.add(`is-size-${fontSize + 1}-mobile`);
+    document.getElementById('content').classList.remove(`is-size-${fontSize}-mobile`);
+    document.getElementById('content').classList.add(`is-size-${fontSize - 1}-mobile`);
 
     fontSize--;
 };
 const fontSizeDown = () => {
-    document.getElementById('title').classList.remove(`is-size-${fontSize + 1}-mobile`);
-    document.getElementById('subtitle').classList.remove(`is-size-${fontSize + 3}-mobile`);
-    document.getElementById('content').classList.remove(`is-size-${fontSize + 2}-mobile`);
+    if (fontSize >= 7) return;
 
-    document.getElementById('title').classList.add(`is-size-${fontSize + 2}-mobile`);
-    document.getElementById('subtitle').classList.add(`is-size-${fontSize + 4}-mobile`);
-    document.getElementById('content').classList.add(`is-size-${fontSize + 3}-mobile`);
+    document.getElementById('content').classList.remove(`is-size-${fontSize}-mobile`);
+    document.getElementById('content').classList.add(`is-size-${fontSize + 1}-mobile`);
 
     fontSize++;
 };
 
+/*===== Go To Top =====*/
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
     document.getElementById('goToTopBtn').style.display =
         document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? 'block' : 'none';
 };
-
 // When the user clicks on the button, scroll to the top of the document
 const topFunction = () => {
     document.body.scrollTop = 0; // For Safari
