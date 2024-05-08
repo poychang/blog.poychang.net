1f95ac743adeffd9d77c5272595c1d8ba6a72908
Poy Chang <poypost@gmail.com>
2024-05-08 16:58:04 +0800

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
   contact_list.mouseleave(function () { scontacts.fadeOut(); });
 
-  // input keyword in sidebar-search-box when key down
+//   // input keyword in sidebar-search-box when key down
 //   document.onkeydown = function (event) {
 //     var azKeySet = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
 //     var AZKeySet = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
@@ -28,4 +28,4 @@ $(document).ready(function () {
 //       }
 //     }
 //   };
-// });
+});
